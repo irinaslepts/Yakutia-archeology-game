299405b9f2c3af21a6f309b88dc0a8d717c903a3
irinaslepts <irina.animation@gmail.com>
2022-11-24 18:33:06 +0900

    1 version

---
+++ Assets/btnScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btnScript : MonoBehaviour
+{
+    public void Change_lvl(string lvl){
+        SceneManager.LoadScene(lvl);
+    }
+
+}
